996dd088d9ab96cf9d552568ff8a2fd0f9a9d06e
JadsonLucena <jadsonlucena@gmail.com>
2021-03-21 21:21:10 -0300

    feat (add): event listener

--- src/Emitter.js
+++ src/Emitter.js
@@ -8,6 +8,24 @@ class Emitter {
 
 	}
 
+	addEventListener(name, callback) {
+
+		if (typeof callback != 'function') {
+
+			throw 'The "listener" argument must be of type function';
+
+		}
+
+		if (!(name in this.#events)) {
+
+			this.#events[name] = []
+
+		}
+
+		this.#events[name].push(callback);
+
+	}
+
 }
 
 export default Emitter;
